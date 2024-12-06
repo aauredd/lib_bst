cadd54fe27b87d2a5639b0898ca828549b922963
aauredd <auredibrahimi@gmail.com>
2024-12-06 11:44:44 +0100

    fixed some error

--- lib_bst_class.cpp
+++ lib_bst_class.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 class Node {
 private:
-    int data;         
-    int weight;       
-    Node* lchild;     
-    Node* rchild;     
+    int data;
+    int weight;
+    Node* lchild;
+    Node* rchild;
 
 public:
-    
+
     Node(int value) : data(value), weight(1), lchild(nullptr), rchild(nullptr) {}
     Node* insertR(int k) {
         if (k == data) {
@@ -30,6 +30,69 @@ public:
         return this;
     }
 
+Node* insertI(int k) {
+        Node* current = this;
+        Node* parent = nullptr;
+
+        while (current != nullptr) {
+            parent = current;
+            if (k == current->data) {
+                current->weight++;
+                return this;
+            } else if (k < current->data) {
+                current = current->lchild;
+            } else {
+                current = current->rchild;
+            }
+        }
+
+        Node* newNode = new Node(k);
+        if (k < parent->data) {
+            parent->lchild = newNode;
+        } else {
+            parent->rchild = newNode;
+        }
+
+        return this;
+    }
+
+    bool searchI(int k) {
+        Node* current = this;
+        while (current != nullptr) {
+            if (current->data == k) {
+                return true;
+            } else if (k < current->data) {
+                current = current->lchild;
+            } else {
+                current = current->rchild;
+            }
+        }
+        return false;
+    }
+
+    bool searchR(int k) {
+    if (this == nullptr) {
+        return false;
+    }
+    if (data == k) {
+        return true;
+    }
+    if (k < data) {
+        if (lchild != nullptr) {
+            return lchild->searchR(k);
+        } else {
+            return false;
+        }
+    } else {
+        if (rchild != nullptr) {
+            return rchild->searchR(k);
+        } else {
+            return false;
+        }
+    }
+}
+
+
     void inOrder() {
         if (lchild != nullptr) {
             lchild->inOrder();
@@ -42,7 +105,7 @@ public:
 
     int getData() const { return data; }
 
-    
+
 };
 
 int main() {
