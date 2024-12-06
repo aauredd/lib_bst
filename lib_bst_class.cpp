cc695566dab23b0512e6354a439ddb612f9886ef
aauredd <auredibrahimi@gmail.com>
2024-12-06 11:41:34 +0100

    Implementato l'overloading dei operatori

--- lib_bst_class.cpp
+++ lib_bst_class.cpp
@@ -10,6 +10,19 @@ private:
 
 public:
 
+    
+    friend ostream& operator<<(ostream &os, const Node &node){
+    os << Node.data <<endl;
+    return os;
+        };
+    }
+    friend istream& operator>>(istream& is, Node*& node) {
+        int value;
+        is>>value;
+    Node->insertR(value);
+        return is;
+    }
+    }
     Node(int value) : data(value), weight(1), lchild(nullptr), rchild(nullptr) {}
 
     Node* insertR(int k) {
@@ -30,6 +43,68 @@ public:
         }
         return this;
     }
+Node* insertI(int k) {
+        Node* current = this;
+        Node* parent = nullptr;
+
+        while (current != nullptr) {
+            parent = current;
+            if (k == current->data) {
+                current->weight++;
+                return this;
+            } else if (k < current->data) {
+                current = current->lchild;
+            } else {
+                current = current->rchild;
+            }
+        }
+
+        Node* newNode = new Node(k);
+        if (k < parent->data) {
+            parent->lchild = newNode;
+        } else {
+            parent->rchild = newNode;
+        }
+
+        return this;
+    }
+
+    bool searchI(int k) {
+        Node* current = this;
+        while (current != nullptr) {
+            if (current->data == k) {
+                return true;
+            } else if (k < current->data) {
+                current = current->lchild;
+            } else {
+                current = current->rchild;
+            }
+        }
+        return false;
+    }
+
+    bool searchR(int k) {
+    if (this == nullptr) {
+        return false;
+    }
+    if (data == k) {
+        return true;
+    } 
+    if (k < data) {
+        if (lchild != nullptr) {
+            return lchild->searchR(k);
+        } else {
+            return false;
+        }
+    } else {
+        if (rchild != nullptr) {
+            return rchild->searchR(k);
+        } else {
+            return false;
+        }
+    }
+}
+
 
     void inOrder() {
         if (lchild != nullptr) {
@@ -54,6 +129,6 @@ int main() {
     root = root->insertR(60);
     root = root->insertR(80);
 
-
+cin>>
     return 0;
 }
